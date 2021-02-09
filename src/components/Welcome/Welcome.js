8df0b63df5f759485d78a1ebb59cb3f735399fc0
mireille tchinda <mireille35@yahoo.com>
2021-02-09 16:09:27 -0500

    initial commit

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { withAsyncAction } from "../../redux/HOCs";
 import Menu from "../menu/Menu";
+import { Link } from "react-router-dom";
 
 
 function Welcome(props) {
@@ -11,7 +12,15 @@ function Welcome(props) {
     } else {
       name = props.name;
     }
-    return <div className="Welcome"> Welcome, {name}!</div>;
+    return( <div className="Welcome"> 
+             <Menu />
+             <button style={{padding: "20px", backgroundColor:"dodgerblue", color:"white"}}><Link to='/'>Log Out</Link> </button><br/>
+             <button>Like</button>
+             <button>Dislike</button><br/>
+              
+             <h3 style={{color:"white"}}>Welcome{name}!</h3> 
+              
+            </div> )
   }
   
 
--- src/components/messages/Messages.js
+++ src/components/messages/Messages.js
@@ -36,6 +36,11 @@ class Messages extends React.Component {
       })
     })
   }
+  // deleteMessageHandler = (messageId) => {
+  //   this.props.deleteMessage(messageId).then(() => {
+  //     this.fetchMessages();
+  //   })
+  // }
 
   handleChange = (event) => {
     let data = {...this.state};
@@ -43,6 +48,10 @@ class Messages extends React.Component {
     data[event.target.name] = event.target.value;   
 
     this.setState(data);
+
+  }
+  reset =() =>{
+    this.setState({messages:[]})
   }
 
   render() {
@@ -54,16 +63,22 @@ class Messages extends React.Component {
         )
       })
     }
+   
+   
 
     return (
-      <div className="Messages">
+   <div className="Messages">
+     <div className="Reset">
+
+     </div>
         <div className="ListMessage">
           {display}
         </div>
         <div className="NewMessage">
           <input name="message" onChange={this.handleChange} value={this.state.message}/>
+      
           <button onClick={this.newMessageHandler}> Send Message </button>
-          <button>Reset</button>
+          <button onClick={this.reset}>Reset</button>
           
         </div>
       </div>
